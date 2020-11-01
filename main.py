b76152929649f0f9d1f157382ecef6739751d4ad
Hunter Mahoney <54244104+MahoneyH@users.noreply.github.com>
2020-11-01 09:18:43 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+master = Tk()
+
+w= Canvas(master, width=200, height = 200)
+w.pack()
+
+#w.create_line(0,0,200,100)
+#w.create_line(0,100,200,0,fill="red", dash=(4,4))
+
+#w.create_rectangle(50,25,150,75, fill="blue")
+
+r1 = w.create_rectangle(10,10,10,10, fill="red")
+
+def keypress(event):
+    x=0
+    y=0
+    if event.char=="a": x = -10
+    elif event.char == "s": y = 10
+    elif event.char == "d": x = 10
+    elif event.char == "w": y = -10
+    w.move(r1, x, y)
+
+master.bind("<Key>", keypress)
+
+master.mainloop()
