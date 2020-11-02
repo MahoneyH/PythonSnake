9d1fbe0d53a76eecff272dfc6763c42d122c602a
Hunter Mahoney <54244104+MahoneyH@users.noreply.github.com>
2020-11-01 20:10:03 -0600

    Update main.py
    new movement properties, and auto move in selected direction added.

--- main.py
+++ main.py
@@ -4,23 +4,44 @@ master = Tk()
 w= Canvas(master, width=200, height = 200)
 w.pack()
 
-#w.create_line(0,0,200,100)
-#w.create_line(0,100,200,0,fill="red", dash=(4,4))
-
-#w.create_rectangle(50,25,150,75, fill="blue")
-
 r1 = w.create_rectangle(10,10,10,10, fill="red")
 
+global xMove
+global yMove
+
+xMove = 10
+yMove = 0
+
+#this class will build the player
+#Requires a need for an array and to keep rack of direction and current coordinates on the grid.
+#movement will need to move to the location of the element in front of it. Follow the array.
+#need to track coordiates, array location, expand on the array, and check if head hits itself.
+class player:
+    x = 10
+
+def move():
+    w.move(r1, xMove, yMove)
+    w.after(250, move)
+
+#This is directional movement for the square, prevents it from reversing direction.
+#Uses global varibles to keep track of the direction it is moving.
 def keypress(event):
-    x=0
-    y=0
-    if event.char=="a": x = -10
-    elif event.char == "s": y = 10
-    elif event.char == "d": x = 10
-    elif event.char == "w": y = -10
-    w.move(r1, x, y)
+    global xMove
+    global yMove
+    if event.char=="a" and xMove!= 10:
+        xMove = -10
+        yMove = 0
+    elif event.char == "s" and yMove!=-10:
+        yMove = 10
+        xMove = 0
+    elif event.char == "d" and xMove!=-10:
+        xMove = 10
+        yMove = 0
+    elif event.char == "w" and yMove!=10:
+        yMove = -10
+        xMove = 0;
 
 master.bind("<Key>", keypress)
-
+move()
 master.mainloop()
 #adding a comment to see what happens.
